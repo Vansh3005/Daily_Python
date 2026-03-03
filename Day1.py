36f2a7ea23a6c3cb92f5e177fa6462f86f944f15
VANSH GOYAL <140587946+Vansh3005@users.noreply.github.com>
2026-03-03 12:18:05 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,9 @@
-print("Hello Python")
+# Take 5 numbers as input and store in list
+
+values = []
+
+for i in range(5):
+    num = int(input("Enter the number"))
+    values.append(num)
+
+print("List is:", values)
