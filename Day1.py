9b0db1e1780a77aca5b370366be3712e15d287cb
VANSH GOYAL <140587946+Vansh3005@users.noreply.github.com>
2026-03-03 13:26:15 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,9 +1,28 @@
 # Take 5 numbers as input and store in list
-
 values = []
-
 for i in range(5):
     num = int(input("Enter the number"))
     values.append(num)
-
 print("List is:", values)
+
+# Find sum and average of list
+print("Total of the list is:",sum(values))
+avg = sum(values)/2
+print("Avg of list is:",avg)
+
+# Find maximum and minimum element
+print("Max value in list:",max(values))
+print("Min value in list:",min(values))
+
+# Reverse list without using reverse()
+print("The reverse of list is:")
+for i in range(len(values)-1,-1,-1):  # len(values)-1,-1,-1
+    rev = values[i]                              # start,stop,move
+    print(rev)
+
+# Check if Number Exists in List
+check = int(input("Check no. is present in list or not:"))
+if check in values:
+    print("Number is exists")
+else:
+    print("Number is not exists")
