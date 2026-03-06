1587a8d84f49ec05af2413fc41185e449b86f106
VANSH GOYAL <140587946+Vansh3005@users.noreply.github.com>
2026-03-06 23:15:53 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,3 +1,10 @@
 # Print the total of the list
-list = [1,2,3,4,5]
+list = [1,2,2,3,4,5,5,6,7]
 print("sum of the list is:",sum(list))
+
+# Remove duplicacy in list
+result = []
+for i in list:
+    if i not in result:
+        result.append(i)
+print("List without duplicacy",result) 
