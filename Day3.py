58e6daf98c061280984cb2b4d7405b5ecc8f219d
VANSH GOYAL <140587946+Vansh3005@users.noreply.github.com>
2026-03-07 23:34:17 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,1 +1,9 @@
-print("This is Day 3")
+# Find all pairs whose sum = target
+list = [1,2,3,4,5,6]
+target = 5
+for i in range(len(list)):
+    for j in range(i+1,len(list)): # (i+1) avoid the duplicacy and comparison with itself
+        if list[i] + list[j] == target:
+            print(list[i], list[j])
+
+
